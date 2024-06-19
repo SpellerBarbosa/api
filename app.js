a93a44320f52a69da1601003c3f5b0fa3b234fb9
SpellerBarbosa <133154923+SpellerBarbosa@users.noreply.github.com>
2024-06-18 23:32:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import fs from 'fs';
+import fs from 'fs/promises';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import { existsSync } from 'fs';
